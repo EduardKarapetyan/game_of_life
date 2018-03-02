72c2ec44e3188203c00acd60b6355143889fc66c
EduardKarapetyan <edo.karapetyan1999@gmail.com>
2018-03-02 22:52:19 +0400

    goz

--- class.js
+++ class.js
@@ -35,7 +35,6 @@ class Grass {
     bazmanal() {
         this.multiply++;
         var norVandak = random(this.yntrelVandak(0));
-        console.log(norVandak, this.multiply);
         if (this.multiply >= 8 && norVandak) {
             var norXot = new Grass(norVandak[0], norVandak[1]);
             grassArr.push(norXot);
@@ -45,4 +44,76 @@ class Grass {
         this.multiply++;
     }
 }
+class Xotaker {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.energy = 8;
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1],
+        ]
+    }
+    stanalNorKordinatner() {
+        this.directions = [
+             [this.x - 1, this.y - 1],
+             [this.x, this.y - 1],
+             [this.x + 1, this.y - 1],
+             [this.x - 1, this.y],
+             [this.x + 1, this.y],
+             [this.x - 1, this.y + 1],
+             [this.x, this.y + 1],
+             [this.x + 1, this.y + 1]
+        ];
+        for (let i of this.directions) {
+            if (matrix[i[0]] && matrix[i[0]][i[1]] && matrix[i[0]][i[1]] == 1) {
+                matrix[this.x][this.y] = 0;
+                this.x = i[0];
+                this.y = i[1];
+                matrix[this.x][this.y] = 2;
+                break;
+            }
+        }
+    }
 
+
+
+    yntrelvandak(ch) {
+        this.stanalNorKordinatner;
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[1].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == ch) {
+                    found.push(this.directions[i]);
+                }
+            }
+
+
+        }
+        return found;
+    }
+
+    sharjvel() {
+        this.stanalNorKordinatner();
+        }
+    
+    bazmanal() {
+        this.energy++;
+        var norVandak = random(this.yntrelVandak(1));
+        if (this.energy >= 8 && norVandak) {
+            var norXotaker = new Xotaker(norVandak[0], norVandak[1]);
+            xotakerArr.push(norXotaker);
+            matrix[norVandak[1]][norVandak[0]] = 2;
+            this.energy = 0;
+        }
+        this.energy++;
+    }
+}
--- script.js
+++ script.js
@@ -1,20 +1,50 @@
-var matrix = [
-    [0, 0, 1, 0, 0],
-    [1, 0, 0, 0, 0],
+var matrix = [[0, 0, 1, 0, 0],
+    [1, 0, 0, 2, 0],
     [0, 1, 0, 0, 0],
     [0, 0, 1, 0, 0],
-    [1, 1, 0, 0, 0],
+    [1, 1, 0, 2, 0],
     [1, 1, 0, 0, 0],
     [1, 1, 0, 0, 0]
 ];
 
+/*
+for (let i = 0; i < 50; i++) {
+    matrix[i] = [];
+    for (let j = 0; j < 50; j++) {
+        matrix[i][j] = 0;
+    }
+}
+
+for (let i = 0; i < 5; i++) {
+    let x = Math.floor(Math.random()*matrix.length);
+    let y = Math.floor(Math.random()*matrix[0].length);
+    if (matrix[x][y] == 1){
+        i--;
+        continue;
+    }
+    matrix[x][y] = 1;
+}
+for (let i = 0; i < 5; i++) {
+    let x = Math.floor(Math.random() * matrix.length);
+    let y = Math.floor(Math.random() * matrix[0].length);
+    if (matrix[x][y] == 2) {
+        i--;
+        continue;
+    }
+    matrix[x][y] = 2;
+}
+*/
+
+
+
 var side = 120;
 var grassArr = [];
+var xotakerArr = [];
 
 
 function setup() {
-    frameRate(5);
-    createCanvas(matrix[0].length * side, matrix.length * side);
+    frameRate(25);
+    createCanvas(matrix.length * side, matrix.length * side);
     background('#acacac');
 
     for (var y = 0; y < matrix.length; y++) {
@@ -23,16 +53,21 @@ function setup() {
             if (matrix[y][x] == 1) {
                 grassArr.push(new Grass(x, y));
             }
+            else if (matrix[y][x] == 2) {
+                xotakerArr.push(new Xotaker(x, y));
+            }
 
 
         }
     }
-    console.log(grassArr);
+
 }
 
 function draw() {
+
     for (var y = 0; y < matrix.length; y++) {
         for (var x = 0; x < matrix[y].length; x++) {
+            side = 1200 / matrix.length;
 
             if (matrix[y][x] == 1) {
                 fill("green");
@@ -42,12 +77,23 @@ function draw() {
                 fill("#acacac");
                 rect(x * side, y * side, side, side);
             }
+            else if (matrix[y][x] == 2) {
+                fill("yellow");
+                rect(x * side, y * side, side, side);
+            }
         }
     }
-    for (var i in grassArr) {
-        grassArr[i].bazmanal();
+    if (frameCount % 6 == 0) {
+        for (var i in grassArr) {
+            grassArr[i].bazmanal();
+        }
     }
-
+    
+    if (frameCount % 5 === 0) {
+        for (var i in xotakerArr) {
+            xotakerArr[i].sharjvel();
+        }
+    }
+   
 }
-
 
